7aaf8500156d059e330a48e8ecd5a2bdeb007080
Martyna Żukowska <68146740+Martynazukowska@users.noreply.github.com>
2021-07-09 00:43:25 +0200

    pierwszy

--- zad1/lab1/main.c
+++ zad1/lab1/main.c
@@ -1,31 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define INIT_LED 	DDRA |= (1<<PA0)  //output - wyjœcie bo nim sterujemy (DDRx gdzie x to nazwa postu)
-#define LED_ON 		PORTA |= (1<<PA0)
-#define LED_OFF 	PORTA &= ~(1<<PA0)
-
-#define INIT_SW  	DDRA &= ~(1<<PA2) // nie ma rezystora wiec pull-up
-#define PULLUP_SW 	PORTA |= (1<<PA2)
-
-
+#define INIT_LED_PA0 DDRA |= (1<<PA0)  //output - wyjœcie bo nim sterujemy (DDRx gdzie x to nazwa postu)
+#define LED_ON PORTA |= (1<<PA0)
+#define LED_OFF PORTA &= ~(1<<PA0)
 
 int main(void)
 {
-	INIT_LED;
-	INIT_SW;
-	PULLUP_SW;
+	INIT_LED_PA0;
 	
 	while(1)
 	{
-		if(!(PINA & (1<<PA2))) //
-		{
-			LED_ON;
-		}
-		else
-		{
-			LED_OFF;
-		}
+		LED_ON;
+		_delay_ms(300);
+		LED_OFF;
+		_delay_ms(300);
 	}
 	return 0;
 
